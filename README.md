8d6d435c7a547a13f8ea12de3d30a2b83ec9433c
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2018-05-17 11:13:55 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+npm fzf installer
+=================
+
+This package installs latest version of fzf.
+
+Tested only on Linux and macOS, pull requests to make it work on other
+platforms are welcome.
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+INSTALL_URL="https://raw.githubusercontent.com/junegunn/fzf/master/install"
+INSTALL_NAME="$( basename "${INSTALL_URL}" )"
+
+FETCH="$(
+    if which curl
+    then
+        echo --progress-bar --output "${INSTALL_NAME}"
+    fi
+)"
+
+if [ -z "${FETCH}" ]
+then
+    echo "Failed to find a program to fetch  Tried: curl, wget"
+    exit 1
+fi
+
+echo "Downloading installer ..."
+${FETCH} "${INSTALL_URL}"
+
+echo "Running installer ..."
+bash "${INSTALL_NAME}" --bin
